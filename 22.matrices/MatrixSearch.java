25d91616da8bc4943ce2a91786eac9cf8f7fd5b4
william Duke <wduke2002@gmail.com>
2019-04-23 14:08:42 -0500

    yee

--- 22.matrices/MatrixSearch.java
+++ 22.matrices/MatrixSearch.java
@@ -7,18 +7,25 @@ import static java.lang.System.*;
 
 public class MatrixSearch
 {
-	/*
-	 *  method searchMat should determine how many
-	 *  occurrences of toFind exist in the matrix mat
-	 */
-	public static int searchMat( int[][] mat, int toFind)
-	{
-		return 0;
-	}
+ /*
+  *  method searchMat should determine how many
+  *  occurrences of toFind exist in the matrix mat
+  */
+ public static int searchMat( int[][] mat, int toFind)
+ {
+   int counter = 0;
+   for(int[] i : mat){
+     for(int k:i){
+       if(k==toFind)
+         counter++;
+     }
+   }
+   return counter;
+ }
 
-	public static void main(String args[])
-	{
-		int[][] twoDRay = {{5,7,7,8,7,8},{5,3,4,6},{0,8,9,1,2,3,4,5}};
-		System.out.println( searchMat( twoDRay, 7) );
-	}
+ public static void main(String args[])
+ {
+  int[][] twoDRay = {{5,7,7,8,7,8},{5,3,4,6},{0,8,9,1,2,3,4,5}};
+  System.out.println( searchMat( twoDRay, 3) );
+ }
 }
--- 22.matrices/MatrixSetTwo.java
+++ 22.matrices/MatrixSetTwo.java
@@ -7,26 +7,32 @@ import static java.lang.System.*;
 
 public class MatrixSetTwo
 {
-	public static void main(String args[])
-	{
-		int[][] mat = new int[5][5];
+ public static void main(String args[])
+ {
+  String[][] mat = new String[10][10];
 
-		for(int r=0; r<mat.length; r++)
-		{
-			for(int c=0; c<mat[0].length; c++)
-			{
-				mat[r][c] = r*c;
-			}
-		}
+  for(int r=0; r<mat.length; r++)
+  {
+   for(int c=0; c<mat[0].length; c++)
+   {
+     if(r==c)
+    mat[r][c] = ""+r*c;
+     
+   }
+   
+  }
 
-		for(int r=0; r<mat.length; r++)
-		{
-			out.print("row "+ r + " ");
-			for(int c=0; c<mat[0].length; c++)
-			{
-				out.printf("%3d",mat[r][c]);
-			}
-			out.println();
-		}
-	}
+  for(int r=0; r<mat.length; r++)
+  {
+   out.print("row "+ r + " ");
+   for(int c=0; c<mat[0].length; c++)
+   {
+     if(mat[r][c]!= null)
+    out.print(mat[r][c]);
+     if(mat[r][c]==null)
+       out.print("    ");
+   }
+   out.println();
+  }
+ }
 }
